49ba1023f6f3a0d3c0d4447efa810e70db8e94c6
nicolas <nicolas.ninin@gmail.com>
2024-12-31 01:17:49 +0100

    another git lfs for render test

--- build.sh
+++ build.sh
@@ -3,8 +3,5 @@
 # Install Python requirements (including git-lfs)
 pip install -r requirements.txt
 
-# Initialize git-lfs
-git lfs install
-
-# Pull LFS files
-git lfs pull
+mkdir models
+curl -L -o models/frWac_no_postag_phrase_500_cbow_cut10_stripped.bin https://github.com/NicolasNin/cemantix_solve/raw/refs/heads/master/models/frWac_no_postag_phrase_500_cbow_cut10_stripped.bin
